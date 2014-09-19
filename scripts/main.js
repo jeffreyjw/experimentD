59677043c767890926df87d8f6310f9cb998c49f
uxdream <uxdream@uxdreams-mini.home>
2014-09-19 17:48:26 +0200

    Zadanie 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 require([
     "networkData",
+    "gaslib/Point",
     "maplib/MapDrawer"
-], function(networkData, MapDrawer){
+], function(networkData, Point, MapDrawer){
 	var mapElement = document.getElementById('map');
 	var drawer = new MapDrawer(mapElement);
+
+	drawer.drawLine(new Point(52.40, 16.90), new Point(52.23, 21.06), {weight: 1, popup: 'trolo', color: 'red', speed: 1});
 });
