e46168377d6c9d29ccf0d8b077212fd99d4bbbbc
uxdream <uxdream@uxdreams-mini.home>
2014-09-19 18:48:40 +0200

    Zadanie 4a

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,17 @@ require([
 	var mapElement = document.getElementById('map');
 	var drawer = new MapDrawer(mapElement);
 
-	drawer.drawLine(new Point(52.40, 16.90), new Point(52.23, 21.06), {weight: 1, popup: 'trolo', color: 'red', speed: 1});
+	//drawer.drawLine(new Point(52.40, 16.90), new Point(52.23, 21.06), {weight: 1, popup: 'trolo', color: 'red', speed: 1});
 
 	network = JSONToGasNetworkConverter(networkData);
+	
+	network.pipes.__collection.forEach( function(pipe){
+		console.log(pipe);
+		drawer.drawLine(pipe.start, pipe.finish, {weight: 1, popup: 'trolo', color: 'red', speed: 1});
+	});
+
+	network.nodes.__collection.forEach( function(node){
+		drawer.drawMarker(node.position, {caption: "caption", content:"content", popup:"popup"});
+	});
+
 });
