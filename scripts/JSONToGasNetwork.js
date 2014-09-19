ac58b7a1e79a4718805633dd4088156ebe244e6d
uxdream <uxdream@uxdreams-mini.home>
2014-09-19 18:34:39 +0200

    Zadanie 4

--- scripts/JSONToGasNetwork.js
+++ scripts/JSONToGasNetwork.js
@@ -1,9 +1,42 @@
 define([
-
-], function(){
+	"gaslib/GasNetwork",
+	"gaslib/Pipe",
+	"gaslib/Node",
+	"gaslib/Angle",
+	"gaslib/Point",
+], function(GasNetwork, Pipe, Node, Angle, Point){
     "use strict";
 
+    var getPoint = function(data){
+	if(data.unit === "deg")
+		return new Point(data.lat, data.lng);
+	else
+		return new Point(Angle.radianToDegree(data.lat), Angle.radianToDegree(data.lng));
+    }
+
     var JSONToGasNetwork = function(data){
+	var network = new GasNetwork();
+	for(var i = 0 ; i< data.pipes.length ; i++){
+		var dataPipe = data.pipes[i];
+		var pipe = new Pipe();
+		pipe.start = getPoint(dataPipe.start);
+		pipe.finish = getPoint(dataPipe.end);
+
+		pipe.flow = dataPipe.flow;		
+		pipe.maxFlow = dataPipe.maxFlow;		
+		network.addPipe(pipe);
+	}
+
+	for(var i = 0 ; i< data.nodes.length ; i++){
+		var dataNode = data.nodes[i];
+		var node = new Node();
+		node.position = getPoint(dataNode);
+		node.setName(dataNode.caption);
+
+		network.addNode(node);
+	}
+
+	return network;
     };
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 require([
     "networkData",
     "gaslib/Point",
-    "maplib/MapDrawer"
-], function(networkData, Point, MapDrawer){
+    "maplib/MapDrawer",
+    "JSONToGasNetwork",
+], function(networkData, Point, MapDrawer, JSONToGasNetworkConverter){
 	var mapElement = document.getElementById('map');
 	var drawer = new MapDrawer(mapElement);
 
 	drawer.drawLine(new Point(52.40, 16.90), new Point(52.23, 21.06), {weight: 1, popup: 'trolo', color: 'red', speed: 1});
+
+	network = JSONToGasNetworkConverter(networkData);
 });
