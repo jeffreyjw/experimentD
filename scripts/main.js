d2c3c5023a1313856dcf8bfc0bc8d3dc931de543
uxdream <uxdream@uxdreams-mini.home>
2014-09-19 17:36:39 +0200

    Zadanie 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 require([
-    "networkData"
-], function(networkData){
-
-    
-
+    "networkData",
+    "maplib/MapDrawer"
+], function(networkData, MapDrawer){
+	var mapElement = document.getElementById('map');
+	var drawer = new MapDrawer(mapElement);
 });
